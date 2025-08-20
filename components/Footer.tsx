545bee59283a25571287ba4ffd6280b0d1f98025
sarigamamusiccorner-cloud <sarigamamusiccorner@gmail.com>
2025-08-20 07:50:14 -1100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,33 +63,35 @@ const Footer: React.FC = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 flex-shrink-0 text-brand-cyan" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                 </svg>
-                                <a href="tel:+919961569470" className="hover:text-brand-light transition-colors">
-                                    +91 9961569470
+                                <a href="tel:+911234567890" className="hover:text-brand-light transition-colors">
+                                    +91 123 456 7890
                                 </a>
                             </li>
                         </ul>
                     </div>
                     
                     {/* Column 4: Social */}
-                    <div className="flex flex-col items-center md:items-start">
+                    <div>
                         <h3 className="font-bold text-brand-light mb-4">Follow Us</h3>
-                        <div className="flex space-x-6">
-                            <SocialIcon href="https://instagram.com/sarigamamusiccorner" label="Instagram">
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.024.06 1.378.06 3.808s-.012 2.784-.06 3.808c-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.024.048-1.378.06-3.808.06s-2.784-.012-3.808-.06c-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.048-1.024-.06-1.378-.06-3.808s.012-2.784.06-3.808c.049 1.064.218 1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 016.343 2.525c.636-.247 1.363-.416 2.427-.465C9.793 2.013 10.147 2 12.315 2zm-1.002 6.37a4.625 4.625 0 100 9.25 4.625 4.625 0 000-9.25z" clipRule="evenodd" /><path d="M16.51 6.375a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" /></svg>
+                        <div className="flex justify-center md:justify-start space-x-6">
+                            <SocialIcon href="https://open.spotify.com/artist/1h3k52p5NotPsgkI5lSj6h" label="Spotify">
+                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4.183 14.226c-.23.363-.695.485-.933.248-2.175-1.432-4.92-1.758-8.24-.957-.456.11-.91-.17-.91-.643.0-.474.453-.752.909-.862 3.715-.89 6.843-.51 9.278 1.135.238.237.363.695.12 1.074zM17.5 13.12c-.28.45-.82.6-1.274.32-2.52-1.55-6.23-2.01-9.45-.98-.54.17-.98-.21-.98-.75s.44-.92.98-.98c3.67-1.02 7.84-.5 10.74 1.32.45.27.6.82.33 1.27zm.12-2.91c-.32.53-1.02.7-1.55.38-2.93-1.79-7.55-2.3-10.4-.98-.64.19-1.28-.28-1.28-.93s.64-1.12 1.28-.93c3.48-1.04 8.71-.4 11.96 1.6.53.32.7 1.02.39 1.56z"/></svg>
                             </SocialIcon>
-                            <SocialIcon href="https://youtube.com/@_smc" label="YouTube">
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.78 22 12 22 12s0 3.22-.42 4.814a2.506 2.506 0 0 1-1.768 1.768c-1.594.42-7.812.42-7.812.42s-6.218 0-7.812-.42a2.506 2.506 0 0 1-1.768-1.768C2 15.22 2 12 2 12s0-3.22.42-4.814a2.506 2.506 0 0 1 1.768-1.768C5.782 5 12 5 12 5s6.218 0 7.812.418zM9.5 15.5V8.5l6 3.5-6 3.5z" clipRule="evenodd" /></svg>
+                            <SocialIcon href="https://youtube.com" label="YouTube">
+                                 <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M21.582 7.042c-.225-.814-.88-1.468-1.697-1.695C18.265 5 12 5 12 5s-6.265 0-7.885.347c-.817.227-1.472.88-1.697 1.695C2 8.658 2 12 2 12s0 3.342.418 4.958c.225.815.88 1.468 1.697 1.695C5.735 19 12 19 12 19s6.265 0 7.885-.347c.817-.227 1.472.88 1.697-1.695C22 15.342 22 12 22 12s0-3.342-.418-4.958zM9.75 15V9l5.25 3-5.25 3z" /></svg>
                             </SocialIcon>
-                            <SocialIcon href="https://spotify.com" label="Spotify">
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm-3.37 14.332a.75.75 0 01-1.06-1.06l3.182-3.182a.75.75 0 111.06 1.06l-3.182 3.182zm1.06-5.414l-3.182 3.182a.75.75 0 01-1.06-1.06L9.63 8.858a.75.75 0 011.06 1.06zm5.228-3.08a.75.75 0 010 1.06l-7.354 7.354a.75.75 0 01-1.06-1.06L14.918 8.838a.75._75 0 011.06 0z" clipRule="evenodd" /></svg>
+                            <SocialIcon href="https://instagram.com" label="Instagram">
+                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.217.598 1.772 1.153a4.908 4.908 0 011.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122s-.013 3.056-.06 4.122c-.05 1.065-.218 1.79-.465 2.428a4.908 4.908 0 01-1.153 1.772 4.908 4.908 0 01-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06s-3.056-.013-4.122-.06c-1.065-.05-1.79-.218-2.428-.465a4.908 4.908 0 01-1.772-1.153 4.908 4.908 0 01-1.153-1.772c-.247-.637-.415-1.363-.465-2.428-.047-1.066-.06-1.405-.06-4.122s.013-3.056.06-4.122c.05-1.065.218-1.79.465-2.428a4.908 4.908 0 011.153-1.772A4.908 4.908 0 016.343 2.52c.637-.247 1.363.415 2.428-.465C9.83 2.01 10.16 2 12 2zm0 4.625a5.375 5.375 0 100 10.75 5.375 5.375 0 000-10.75zm0 8.875a3.5 3.5 0 110-7 3.5 3.5 0 010 7zm4.875-9.875a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5z" /></svg>
                             </SocialIcon>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="bg-brand-dark">
-                <div className="container mx-auto px-6 py-4 text-center text-sm text-gray-500">
-                    © {new Date().getFullYear()} Sarigama Music Corner. All Rights Reserved.
+
+                {/* Copyright */}
+                <div className="mt-12 pt-8 border-t border-brand-cyan/20 text-center">
+                    <p className="text-gray-500 text-sm">
+                        &copy; {new Date().getFullYear()} Sarigama Music Corner. All Rights Reserved.
+                    </p>
                 </div>
             </div>
         </footer>
